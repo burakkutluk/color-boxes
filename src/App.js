87db367a2dbf072496d0642a347f2250926205e5
19691473550 <burakkutlukk@gmail.com>
2023-11-29 15:53:42 +0300

    colorBox

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BoxContainer from './components/BoxContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BoxContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Box from './Box'
+import './BoxContainer.css'
+
+class BoxContainer extends Component {
+    static defaultProps = {
+        numBoxes: 28,
+        allColors: [
+            'purple',
+            'magenta',
+            'brown',
+            'lilac',
+            'pink',
+            'orange',
+            'violet',
+            'red',
+            'blue',
+            'green',
+        ]
+    }
+
+
+
+  render() {
+    const boxes = Array.from({length: this.props.numBoxes}).map(()=>(
+        <Box colors = {this.props.allColors}/>
+    ))
+    return (
+      <div className='BoxContainer'>
+        {boxes}
+      </div>
+    )
+  }
+}
+export default BoxContainer
